c98de17496d2ae12f51e41eef52d9e4efe522c65
arryllopez <arryllopez7@gmail.com>
2025-01-13 12:55:34 -0500

    imported matplotlib to plot the counts of fake and true news in the combined datasets

--- fakeNewsDetection.py
+++ fakeNewsDetection.py
@@ -1,6 +1,7 @@
 #importing the necessary libraries
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 #loading the dataset
 fake_dataset = pd.read_csv('archive/datasets/Fake.csv')
@@ -19,3 +20,30 @@ true_dataset["label"] = "true"
 #testin if label is applied
 print(fake_dataset.head())
 print(true_dataset.head())
+
+
+#shuffling the rows of each dataset so that the combined data set is randomized between false and true 
+combined_data = pd.concat ([fake_dataset, true_dataset])
+combined_data = combined_data.sample(n=len(combined_data), random_state=None).reset_index(drop=True) 
+combined_data
+
+#printing to see if it is shuffled
+print(combined_data)
+
+
+#checking to see if there are any empty values in the columns
+print(combined_data.isnull().sum()) 
+
+#number of fake pieces of data and number of pieces of true data
+print(combined_data.label.value_counts()) 
+
+#matplot plotting the count of fake vs real news
+label_counts=combined_data.label.value_counts()
+plt.figure(figsize=(6,4))
+#describing the style of the plot an the colors 
+label_counts.plot(kind='bar', color=['red', 'green'])
+plt.title('Count of Fake vs Real News', fontsize=14)
+plt.xlabel('Label', fontsize=12)
+plt.ylabel('Count', fontsize=12)
+#showing the graph 
+plt.show() 
