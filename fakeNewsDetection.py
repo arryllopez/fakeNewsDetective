87ce259e016f258d8df14585b74d9cbc2ec60bca
arryllopez <arryllopez7@gmail.com>
2025-01-12 21:13:03 -0500

    import libraries and load dataset

--- fakeNewsDetection.py
+++ fakeNewsDetection.py
@@ -1,1 +1,10 @@
+#importing the necessary libraries
 import pandas as pd
+import numpy as np
+
+#loading the dataset
+fake_dataset = pd.read_csv('archive/datasets/Fake.csv')
+true_dataset = pd.read_csv('archive/datasets/True.csv')
+
+#testing to see if loading is succesful 
+print (fake_dataset.head())
