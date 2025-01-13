424ffd8ef59f439f5562cc52a3af9b707e69b6c7
arryllopez <arryllopez7@gmail.com>
2025-01-12 21:16:51 -0500

    analysing the data and assigning constant values

--- fakeNewsDetection.py
+++ fakeNewsDetection.py
@@ -8,3 +8,14 @@ true_dataset = pd.read_csv('archive/datasets/True.csv')
 
 #testing to see if loading is succesful 
 print (fake_dataset.head())
+print (true_dataset.head())
+print(fake_dataset.describe(include='object'))
+print(true_dataset.describe(include='object'))
+
+#adding a new column assigning a constant value to each dataset 
+fake_dataset["label"] = "fake"
+true_dataset["label"] = "true"
+
+#testin if label is applied
+print(fake_dataset.head())
+print(true_dataset.head())
